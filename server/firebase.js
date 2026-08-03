727621af74ad16682c4a5ccbb101b3c0d8cc00ec
ange <faulkneremelia@gmail.com>
2026-08-03 17:17:32 -0400

    Remove hardcoded keyFilename, use GOOGLE_APPLICATION_CREDENTIALS

--- server/firebase.js
+++ server/firebase.js
@@ -1,3 +1,4 @@
+/*
 const { Firestore } = require('@google-cloud/firestore');
 const path = require('path');
 
@@ -5,6 +6,15 @@ const keyPath = path.join(__dirname, 'serviceAccountKey.json');
 
 const db = new Firestore({
   keyFilename: keyPath,
+  projectId: 'condition-survey-9b5b9'
+});
+
+module.exports = db;
+*/
+
+const { Firestore } = require('@google-cloud/firestore');
+
+const db = new Firestore({
   projectId: 'condition-survey-9b5b9'
 });
 
